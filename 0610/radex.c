873ab69dba717e61e6a401bd586a2c1c46cda5a3
njj0000 <62435211+njj0000@users.noreply.github.com>
2020-06-10 10:59:48 +0900

    0610

---
+++ 0610/radex.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	int a = 6;
+	printf("hexa = %x octal = %o deciaml = %d integer = %i\n",a,a,a,a);
+	return 0;
+}
---
+++ 0610/recursive1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int fact(int a)
+{
+	printf("%d\n",a);
+	if(a ==1)
+	return 1;
+	else 
+	return a * fact(a-1);
+	printf("%d\n",a);
+	
+}
+
+
+
+int main()
+{
+	int a,n;
+	printf("숫자입력");
+	scanf("%d",&n); 
+	a= fact(n);
+	
+	printf("%d!=%d\n",n,a);
+	
+	
+	
+	
+	return 0;
+}
